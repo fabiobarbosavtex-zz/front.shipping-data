34173e41559c181f4e03850ddc15e537bd80533e
Breno Calazans <breno@vtex.com.br>
2016-04-27 20:25:18 -0300

    Add support to Portugal

---
+++ src/script/translation/pt-PT.js
@@ -0,0 +1,21 @@
+(function(){
+var define = vtex.define || window.define;
+
+define(['shipping/script/translation/pt-BR'], function (ptBRTranslation) {
+  return $.extend(true, {}, ptBRTranslation, {
+    "shipping": {
+      "addressForm": {
+        "addressLine1": 'Morada Linha 1',
+        "addressLine2": 'Morada Linha 2',
+      },
+      "addressSearch": {
+        "dontKnowPostalCode": 'Não sei meu código postal',
+        "knowPostalCode": 'Buscar pelo meu código postal'
+      },
+      "validation": {
+        "postalcode": 'Informe um CEP válido.'
+      }
+    }
+  });
+});
+})();
